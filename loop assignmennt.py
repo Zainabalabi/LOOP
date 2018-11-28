d968856730b25fd99e4e24a0ffd4682ddfe002bf
Zainabalabi <45189842+Zainabalabi@users.noreply.github.com>
2018-11-28 14:19:02 +0100

    Add files via upload

---
+++ loop assignmennt.py
@@ -0,0 +1,21 @@
+'''
+x=1
+while(x<=100):
+    if(x%3==0):
+        print(x,"fizz")
+    elif(x%5==0):
+        print(x,"buzz")
+    else:
+        (x%3,5==0)
+        print(x,"fizzbuzz")
+    x+=1
+    '''
+x=1
+while(x<=100):
+    if(x%3==0) and (x%5==0):
+        print(x,"fizzbuzz")
+    elif(x%3==0):
+        print(x,"fizz")
+    elif(x%5==0):
+        print(x,"buzz")
+    x+=1
